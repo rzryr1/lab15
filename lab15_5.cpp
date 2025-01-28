1ae6b76eb3497d584e4ec13e96d7ec34e8b67db6
rzryr1 <solotogre@gmail.com>
2025-01-28 17:12:40 +0700

    lab15_5.cpp completed

--- lab15_5.cpp
+++ lab15_5.cpp
@@ -20,3 +20,15 @@ int main(){
 
 //Do not modify the code above this line
 //Write definition of shuffle() using pointer after this line. 
+void shuffle(int *a,int *b,int *c,int *d){
+	int money[4]={*a,*b,*c,*d};
+	for(int i = 0; i < 4;i++){
+		int j = rand()%(i+1) ;
+		swap(money[i],money[j]);
+	}
+	*a = money[0];
+	*b = money[1];
+	*c = money[2];
+	*d = money[3];
+
+}
